6458482efa13958f395f3a922fffe29a7e3429f6
MarkRywell <gopro855@gmail.com>
2023-12-20 20:53:47 +0800

    refactor: :seedling: Update usertypes seed

--- database/seeders/UserTypeSeeder.php
+++ database/seeders/UserTypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\UserType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class UserTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $usertypes = [
+            [
+                'usertype_name' => 'Administrator',
+            ],
+            [
+                'usertype_name' => 'User',
+            ]
+        ];
+
+        foreach ($usertypes as $usertype) {
+            UserType::create($usertype);
+        }
     }
 }
